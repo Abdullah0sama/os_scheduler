08ccf5674040a40f5948e6c65a04834b3dc4b72f
ao <abdullahosama15@gmail.com>
2022-03-31 05:56:46 +0200

    Making functions and adding comparator for arrival time

--- src/Process.cpp
+++ src/Process.cpp
@@ -8,18 +8,18 @@ Process::Process(std::string name, uint arrivalTime, uint burstTime, uint priori
     this -> priority = priority;
 }
 
-uint Process::getBurstTime() {
+uint Process::getBurstTime() const{
     return burstTime;
 }
 
-uint Process::getArrivalTime() {
+uint Process::getArrivalTime() const{
     return arrivalTime;
 }
 
-uint Process::getPriority() {
+uint Process::getPriority() const{
     return priority;
 }
 
-std::string Process::getName() {
+std::string Process::getName() const{
     return name; 
 }
--- src/Process.h
+++ src/Process.h
@@ -12,10 +12,17 @@ private:
     uint priority;
 public:
     Process(std::string name, uint arrivalTime = 0, uint burstTime = 0, uint priority = 0);
-    std::string getName();
-    uint getArrivalTime();
-    uint getBurstTime();
-    uint getPriority();
+    std::string getName() const;
+    uint getArrivalTime() const;
+    uint getBurstTime() const;
+    uint getPriority() const;
+};
+
+
+struct arrivalTime_LessThan {
+    bool operator () (const Process& p1, const Process& p2) const {
+        return p1.getArrivalTime() < p2.getArrivalTime();
+    }
 };
 
 
